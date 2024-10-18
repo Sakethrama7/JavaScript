15e3beb8ec0301863fb0c49399ab3fffdd150c0a
Sakethrama7 <154833885+Sakethrama7@users.noreply.github.com>
2024-10-18 18:28:37 +0000

    container config done

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // By writing this it will treat all JS code as newer version.
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;  // here variable is declared but value is not initialised. so it's value is undefined.
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
